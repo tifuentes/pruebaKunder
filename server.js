1d41bed0a430770837166e13c729c53b4a13a45c
Tomás Fuentes <fuentes.tomas95@gmail.com>
2020-11-17 02:54:05 -0300

    deploying to heroku

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/kunder-exam-frontend'));
+app.use(express.static('/dist/kunder-exam-frontend'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/kunder-exam-frontend/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/kunder-exam-frontend/'});
 });
 
 // Start the app by listening on the default Heroku port
